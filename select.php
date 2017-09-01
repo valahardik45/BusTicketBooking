3aefe5c4bc3b786abdda833a9ce27c4eec30e7cb
Hardik Vala <valahardik45@outlook.com>
2017-09-01 23:28:23 +0530

    updated to deselect the button and remove text.

--- select.php
+++ select.php
@@ -8,16 +8,82 @@ include("main.php");
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>select seat</title>
 <link rel="stylesheet" type="text/css" href="select_style.css" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="./jquery.min.js"></script>
+<style>
+	.selected{
+		background-color:red !important;
+	}
+</style>
 <script>
 $(document).ready(function(){
 	var count = 0;
 	$(".btn").on("click",function(){
-		if(count<2){
-			var str = $("#selectedSeat").text();
-			$("#selectedSeat").html(str + this.id);
-			$(this).css("background-color","red");
-			count++;
+		var valu = this.id;
+		var str2 = "";
+		var str = $("#selectedSeat").text();
+		str = $.trim(str);
+		$("#selectedSeat").html(str);
+		if(str.substring(0,2)==', '){
+			$("#selectedSeat").html(str.substring(2));
+		}
+		if(count<5){
+			if($.trim(str) == ''){
+				$("#selectedSeat").html(str + this.id);
+			}
+			else {
+				var content = $("#selectedSeat").text();
+				if(content.indexOf(valu)>=0){
+					if(content.indexOf(valu+', ')>=0){
+						var contnt = content.replace((valu+', '),' ');
+						$("#selectedSeat").html(contnt);
+					}
+					else{
+						var contnt = content.replace(valu,' ');
+						$("#selectedSeat").html(contnt);
+					}
+				}
+				else{
+					$("#selectedSeat").html(str + ", " + this.id);
+				}
+				
+			}
+			if($(this).attr("class")=="btn selected"){
+				$(this).removeClass("selected");
+				count--;
+			}
+			else if($(this).attr("class")=="btn"){
+				$(this).addClass("selected");
+				count++;
+			}
+		}
+		else if($(this).attr("class")=="btn selected"){
+			$(this).removeClass("selected");
+			count--;
+			if(str == ''){
+				$("#selectedSeat").html(str + this.id);
+			}
+			else {
+				var content = $("#selectedSeat").text();
+				if(content.indexOf(valu)>=0){
+					if(content.indexOf(valu+', ')>=0){
+						var contnt = content.replace((valu+', '),' ');
+						$("#selectedSeat").html(contnt);
+					}
+					else{
+						var contnt = content.replace(valu,' ');
+						$("#selectedSeat").html(contnt);
+					}
+				}
+				else{
+					$("#selectedSeat").html(str + ", " + this.id);
+				}
+				
+			}
+		}
+		str = $("#selectedSeat").text();
+		str = $.trim(str);
+		if(str.substring(str.length-1)==','){
+			$("#selectedSeat").html(str.substring(0,str.length-1));
 		}
 	});
 });
@@ -25,109 +91,107 @@ $(document).ready(function(){
 </head>
 <body>
 <form method="post">
-<div class="a">
-<table>
-<tr>
-<td><input type="button" value="1"  class="btn" id="&nbsp;&nbsp;1"  /></td>
-<td><input type="button" value="2"  class="btn" id="&nbsp;&nbsp;,2"  /></td>
-<td>&nbsp; </td>
-<td><input type="button" value="3"  class="btn" id="&nbsp;&nbsp;,3"  /></td>
-<td><input type="button" value="4"  class="btn" id="&nbsp;&nbsp;4,"  /></td>
+	<div class="a">
+		<table>
+			<tr>
+				<td><input type="button" value="1"  class="btn" id="btn1"  /></td>
+				<td><input type="button" value="2"  class="btn" id="btn2"  /></td>
+				<td>&nbsp; </td>
+				<td><input type="button" value="3"  class="btn" id="btn3"  /></td>
+				<td><input type="button" value="4"  class="btn" id="btn4"  /></td>
+			</tr>
+
+			<tr>
+				<td><input type="button" value="5"  class="btn" id="btn5"  /></td>
+				<td><input type="button" value="6"  class="btn" id="btn6"  /></td>
+				<td>&nbsp; </td>
+				<td><input type="button" value="7"  class="btn" id="btn7"  /></td>
+				<td><input type="button" value="8"  class="btn" id="btn8"  /></td>
 </tr>
 
 <tr>
-<td><input type="button" value="5"  class="btn" id="&nbsp;&nbsp;5,"  /></td>
-<td><input type="button" value="6"  class="btn" id="&nbsp;&nbsp;6,"  /></td>
+<td><input type="button" value="9"  class="btn" id="btn9"  /></td>
+<td><input type="button" value="10"  class="btn" id="btn10"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="7"  class="btn" id="&nbsp;&nbsp;7,"  /></td>
-<td><input type="button" value="8"  class="btn" id="&nbsp;&nbsp;8,"  /></td>
+<td><input type="button" value="11"  class="btn" id="btn11"  /></td>
+<td><input type="button" value="12"  class="btn" id="btn12"  /></td>
+<td>&nbsp; </td>
+<td> selected seat</td>
 </tr>
 
 <tr>
-<td><input type="button" value="9"  class="btn" id="&nbsp;&nbsp;9,"  /></td>
-<td><input type="button" value="10"  class="btn" id="&nbsp;&nbsp;10,"  /></td>
+<td><input type="button" value="13"  class="btn" id="btn13"  /></td>
+<td><input type="button" value="14"  class="btn" id="btn14"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="11"  class="btn" id="&nbsp;&nbsp;11,"  /></td>
-<td><input type="button" value="12"  class="btn" id="&nbsp;&nbsp;12,"  /></td>
-<td>&nbsp; </td>
-<td> selected seat<label id="selectedSeat"></label></td>
-</tr>
-
-<tr>
-<td><input type="button" value="13"  class="btn" id="&nbsp;&nbsp;13,"  /></td>
-<td><input type="button" value="14"  class="btn" id="&nbsp;&nbsp;14,"  /></td>
-<td>&nbsp; </td>
-<td><input type="button" value="15"  class="btn" id="&nbsp;&nbsp;15,"  /></td>
-<td><input type="button" value="16"  class="btn" id="&nbsp;&nbsp;16,"  /></td>
-<td>&nbsp; </td>
-
+<td><input type="button" value="15"  class="btn" id="btn15"  /></td>
+<td><input type="button" value="16"  class="btn" id="btn16"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="17"  class="btn" id="&nbsp;&nbsp;17,"  /></td>
-<td><input type="button" value="18"  class="btn" id="&nbsp;&nbsp;18,"  /></td>
+<td><input type="button" value="17"  class="btn" id="btn17"  /></td>
+<td><input type="button" value="18"  class="btn" id="btn18"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="19"  class="btn" id="&nbsp;&nbsp;19,"  /></td>
-<td><input type="button" value="20"  class="btn" id="&nbsp;&nbsp;20,"  /></td>
+<td><input type="button" value="19"  class="btn" id="btn19"  /></td>
+<td><input type="button" value="20"  class="btn" id="btn20"  /></td>
 <td><input type="button" value="book" name="cmdbook" /></td></tr>
 <tr>
-<td><input type="button" value="21"  class="btn" id="&nbsp;&nbsp;21,"  /></td>
-<td><input type="button" value="22"  class="btn" id="&nbsp;&nbsp;22,"  /></td>
+<td><input type="button" value="21"  class="btn" id="btn21"  /></td>
+<td><input type="button" value="22"  class="btn" id="btn22"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="23"  class="btn" id="&nbsp;&nbsp;23,"  /></td>
-<td><input type="button" value="24"  class="btn" id="&nbsp;&nbsp;24,"  /></td>
+<td><input type="button" value="23"  class="btn" id="btn23"  /></td>
+<td><input type="button" value="24"  class="btn" id="btn24"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="25"  class="btn" id="&nbsp;&nbsp;25,"  /></td>
-<td><input type="button" value="26"  class="btn" id="&nbsp;&nbsp;26,"  /></td>
+<td><input type="button" value="25"  class="btn" id="btn25"  /></td>
+<td><input type="button" value="26"  class="btn" id="btn26"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="27"  class="btn" id="&nbsp;&nbsp;27,"  /></td>
-<td><input type="button" value="28"  class="btn" id="&nbsp;&nbsp;28,"  /></td>
+<td><input type="button" value="27"  class="btn" id="btn27"  /></td>
+<td><input type="button" value="28"  class="btn" id="btn28"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="29"  class="btn" id="&nbsp;&nbsp;29,"  /></td>
-<td><input type="button" value="30"  class="btn" id="&nbsp;&nbsp;30,"  /></td>
+<td><input type="button" value="29"  class="btn" id="btn29"  /></td>
+<td><input type="button" value="30"  class="btn" id="btn30"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="31"  class="btn" id="&nbsp;&nbsp;31,"  /></td>
-<td><input type="button" value="32"  class="btn" id="&nbsp;&nbsp;32,"  /></td>
+<td><input type="button" value="31"  class="btn" id="btn31"  /></td>
+<td><input type="button" value="32"  class="btn" id="btn32"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="33"  class="btn" id="&nbsp;&nbsp;33,"  /></td>
-<td><input type="button" value="34"  class="btn" id="&nbsp;&nbsp;34,"  /></td>
+<td><input type="button" value="33"  class="btn" id="btn33"  /></td>
+<td><input type="button" value="34"  class="btn" id="btn34"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="35"  class="btn" id="&nbsp;&nbsp;35,"  /></td>
-<td><input type="button" value="36"  class="btn" id="&nbsp;&nbsp;36,"  /></td>
+<td><input type="button" value="35"  class="btn" id="btn35"  /></td>
+<td><input type="button" value="36"  class="btn" id="btn36"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="37"  class="btn" id="&nbsp;&nbsp;37,"  /></td>
-<td><input type="button" value="38"  class="btn" id="&nbsp;&nbsp;38,"  /></td>
+<td><input type="button" value="37"  class="btn" id="btn37"  /></td>
+<td><input type="button" value="38"  class="btn" id="btn38"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="39"  class="btn" id="&nbsp;&nbsp;39,"  /></td>
-<td><input type="button" value="40"  class="btn" id="&nbsp;&nbsp;40,"  /></td>
+<td><input type="button" value="39"  class="btn" id="btn39"  /></td>
+<td><input type="button" value="40"  class="btn" id="btn40"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="41"  class="btn" id="&nbsp;&nbsp;41,"  /></td>
-<td><input type="button" value="42"  class="btn" id="&nbsp;&nbsp;42,"  /></td>
+<td><input type="button" value="41"  class="btn" id="btn41"  /></td>
+<td><input type="button" value="42"  class="btn" id="btn42"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="43"  class="btn" id="&nbsp;&nbsp;43,"  /></td>
-<td><input type="button" value="44"  class="btn" id="&nbsp;&nbsp;44,"  /></td>
+<td><input type="button" value="43"  class="btn" id="btn43"  /></td>
+<td><input type="button" value="44"  class="btn" id="btn44"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="45"  class="btn" id="&nbsp;&nbsp;45,"  /></td>
-<td><input type="button" value="46"  class="btn" id="&nbsp;&nbsp;46,"  /></td>
+<td><input type="button" value="45"  class="btn" id="btn45"  /></td>
+<td><input type="button" value="46"  class="btn" id="btn46"  /></td>
 <td>&nbsp; </td>
-<td><input type="button" value="47"  class="btn" id="&nbsp;&nbsp;47,"  /></td>
-<td><input type="button" value="48"  class="btn" id="&nbsp;&nbsp;48,"  /></td>
+<td><input type="button" value="47"  class="btn" id="btn47"  /></td>
+<td><input type="button" value="48"  class="btn" id="btn48"  /></td>
 </tr>
 <tr>
-<td><input type="button" value="49"  class="btn" id="&nbsp;&nbsp;49,"  /></td>
-<td><input type="button" value="50"  class="btn" id="&nbsp;&nbsp;50,"  /></td>
-<td><input type="button" value="51"  class="btn" id="&nbsp;&nbsp;51,"  /></td>
-<td><input type="button" value="52"  class="btn" id="&nbsp;&nbsp;52,"  /></td>
-<td><input type="button" value="53"  class="btn" id="&nbsp;&nbsp;53,"  /></td>
+<td><input type="button" value="49"  class="btn" id="btn49"  /></td>
+<td><input type="button" value="50"  class="btn" id="btn50"  /></td>
+<td><input type="button" value="51"  class="btn" id="btn51"  /></td>
+<td><input type="button" value="52"  class="btn" id="btn52"  /></td>
+<td><input type="button" value="53"  class="btn" id="btn53"  /></td>
 </tr>
 </table>
 <label id="selectedSeat"></label>
-
+</div>
 </form>
 
 </body>
